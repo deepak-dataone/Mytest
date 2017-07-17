596f80962558acbccf834bffb448520ec74cf4d2
EMERIOCORP\Deepak0S <deepak.somasekhar@emeriocorp.com>
2017-07-17 14:30:24 +0800

    Update the Default Msg

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Welcome to Laravel
+                    Welcome to Laravel - Deepak
                 </div>
 
                 <div class="links">
